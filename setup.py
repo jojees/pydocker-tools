52de590e67563fb0fb9514069e378d38c2717b92
Joji Vithayathil Johny <joji@jojees.net>
2017-03-31 12:44:59 +0530

    Initial Prototype

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pydocker-tools',
+    version='0.0.1'
+    description='pydocker-tools is a set of tools to work around lengthy or piped command line tools for docker',
+    licence='MIT',
+    author='Joji Vithayathil Johny',
+    author_email='joji@jojees.net',
+
+    packages=find_packages(
+        exclude=['tests']
+    ),
+
+    test_suite='tests',
+
+    entry_points={
+        'console_scripts': [
+            'jpy-dtools = pydockertools.jpydtools:main'
+        ]
+    },
+)
